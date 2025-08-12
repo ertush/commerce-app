d39f568dc6e5f1804ce5ccb4c0df87a9559af7d6
ertush <erotush77@gmail.com>
2025-08-12 12:56:36 +0300

    Added product handler

---
+++ internal/handlers/product.go
@@ -0,0 +1,179 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"commerce-app/internal/database"
+	"commerce-app/internal/models"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+)
+
+type ProductHandler struct {
+	productRepo  *database.ProductRepository
+	categoryRepo *database.CategoryRepository
+}
+
+func NewProductHandler() *ProductHandler {
+	return &ProductHandler{
+		productRepo:  &database.ProductRepository{},
+		categoryRepo: &database.CategoryRepository{},
+	}
+}
+
+// CreateProduct creates a new product
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+	if err := json.NewDecoder(r.Body).Decode(&product); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.productRepo.Create(&product); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+}
+
+// GetProduct gets a product by ID
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := uuid.Parse(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	product, err := h.productRepo.GetByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(product)
+}
+
+// GetAllProducts gets all products
+func (h *ProductHandler) GetAllProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := h.productRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+// GetProductsByCategory gets products by category
+func (h *ProductHandler) GetProductsByCategory(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	categoryID, err := uuid.Parse(vars["categoryId"])
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	products, err := h.productRepo.GetByCategory(categoryID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+// GetAveragePriceByCategory gets average price for a category
+func (h *ProductHandler) GetAveragePriceByCategory(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	categoryID, err := uuid.Parse(vars["categoryId"])
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	categoryPrice, err := h.productRepo.GetAveragePriceByCategory(categoryID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(categoryPrice)
+}
+
+// CreateCategory creates a new category
+func (h *ProductHandler) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var category models.Category
+	if err := json.NewDecoder(r.Body).Decode(&category); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := h.categoryRepo.Create(&category); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(category)
+}
+
+// GetCategory gets a category by ID
+func (h *ProductHandler) GetCategory(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := uuid.Parse(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid category ID", http.StatusBadRequest)
+		return
+	}
+
+	category, err := h.categoryRepo.GetByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(category)
+}
+
+// GetAllCategories gets all categories
+func (h *ProductHandler) GetAllCategories(w http.ResponseWriter, r *http.Request) {
+	categories, err := h.categoryRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(categories)
+}
+
+// GetCategoryChildren gets children of a category
+func (h *ProductHandler) GetCategoryChildren(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	parentID, err := uuid.Parse(vars["parentId"])
+	if err != nil {
+		http.Error(w, "Invalid parent category ID", http.StatusBadRequest)
+		return
+	}
+
+	categories, err := h.categoryRepo.GetChildren(parentID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(categories)
+}
