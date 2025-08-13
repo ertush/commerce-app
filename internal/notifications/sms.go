59b95e964d7fa53ac35f69ada7283a864484fa87
ertush <erotush77@gmail.com>
2025-08-13 15:16:46 +0300

    Updated the sms notification service

--- internal/notifications/sms.go
+++ internal/notifications/sms.go
@@ -2,77 +2,52 @@ package notifications
 
 import (
 	"fmt"
-	"net/http"
-	"net/url"
 	"os"
-	"strings"
+
+	"github.com/AndroidStudyOpenSource/africastalking-go/sms"
 )
 
 // SMSService handles SMS notifications
 type SMSService struct {
-	APIKey   string
-	Username string
-	BaseURL  string
+	APIKey    string
+	Username  string
+	SenderID  string
+	Recipient string
 }
 
 // NewSMSService creates a new SMS service instance
 func NewSMSService() *SMSService {
-	apiKey := getEnv("AFRICASTALKING_API_KEY", "sandbox")
-	username := getEnv("AFRICASTALKING_USERNAME", "sandbox")
-	baseURL := getEnv("AFRICASTALKING_BASE_URL", "https://api.sandbox.africastalking.com")
+	var (
+		apiKey    string = getEnv("AFRICASTALKING_API_KEY", "sandbox")
+		username  string = getEnv("AFRICASTALKING_USERNAME", "sandbox")
+		senderId  string = getEnv("AFRICASTALKING_SENDER_ID", "africas-talking")
+		recipient string = getEnv("AFRICASTALKING_RECIPIENT", "+254700000000")
+	)
 
 	return &SMSService{
-		APIKey:   apiKey,
-		Username: username,
-		BaseURL:  baseURL,
+		APIKey:    apiKey,
+		Username:  username,
+		SenderID:  senderId,
+		Recipient: recipient,
 	}
-}
-
-// SendSMS sends an SMS message
-func (s *SMSService) SendSMS(phoneNumber, message string) error {
-	// Remove any non-digit characters from phone number
-	phoneNumber = strings.ReplaceAll(phoneNumber, " ", "")
-	phoneNumber = strings.ReplaceAll(phoneNumber, "-", "")
-	phoneNumber = strings.ReplaceAll(phoneNumber, "+", "")
-
-	// Add country code if not present
-	if !strings.HasPrefix(phoneNumber, "254") {
-		phoneNumber = "254" + phoneNumber
-	}
-
-	data := url.Values{}
-	data.Set("username", s.Username)
-	data.Set("to", phoneNumber)
-	data.Set("message", message)
-
-	req, err := http.NewRequest("POST", s.BaseURL+"/version1/messaging", strings.NewReader(data.Encode()))
-	if err != nil {
-		return fmt.Errorf("error creating request: %v", err)
-	}
-
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("apiKey", s.APIKey)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("error sending SMS: %v", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("SMS API returned status: %d", resp.StatusCode)
-	}
-
-	return nil
 }
 
 // SendOrderNotification sends an SMS notification for a new order
-func (s *SMSService) SendOrderNotification(phoneNumber, orderID, customerName string, total float64) error {
-	message := fmt.Sprintf("Hello %s! Your order #%s has been placed successfully. Total: $%.2f. Thank you for shopping with us!",
-		customerName, orderID, total)
+func (s *SMSService) SendOrderNotification(message string) error {
 
-	return s.SendSMS(phoneNumber, message)
+	env := "sandbox"
+
+	smsService := sms.NewService(s.Username, s.APIKey, env)
+
+	smsResponse, err := smsService.Send(s.SenderID, s.Recipient, message)
+	if err != nil {
+		fmt.Printf("Failed to send SMS notification: %v\n", err)
+		return err
+	}
+
+	fmt.Printf("SMS notification sent: %v\n", smsResponse)
+
+	return nil
 }
 
 // getEnv gets environment variable with fallback
