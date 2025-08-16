8a353a5f88cefe4c186f1bd61b439ce4cec7b15c
ertush <erotush77@gmail.com>
2025-08-16 21:13:44 +0300

    Updated scripts/deploy-vps.sh

--- scripts/deploy-vps.sh
+++ scripts/deploy-vps.sh
@@ -71,10 +71,10 @@ setup_nginx() {
 
         location / {
             proxy_pass $localhost;  # Your Minikube NodePort
-            proxy_set_header Host $http_host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header Host \$http_host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
         }
     }
 
@@ -87,10 +87,10 @@ setup_nginx() {
 
         location / {
             proxy_pass $localhost;  # Your Minikube NodePort
-            proxy_set_header Host $http_host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header Host \$http_host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
         }
     }
 
